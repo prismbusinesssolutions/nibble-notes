2a1c1de7aa9bb3141474dfedda0d2ea7aabf9eeb
prismbusinesssolutions <prism.business.solutions1@gmail.com>
2026-03-09 00:18:42 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,6 @@
-
-const CACHE = "nibble-notes-v1";
-const urls = ["/","/index.html","/styles.css","/app.js","/manifest.json"];
-
-self.addEventListener("install", e=>{
-e.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(urls)));
+self.addEventListener('install',e=>{
+self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener('fetch',e=>{
 });
